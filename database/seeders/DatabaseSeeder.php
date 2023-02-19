90ffac01c69049614fe028f978bfb9112ba4b158
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-02-19 00:32:21 -0500

    Models, factories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,16 +7,21 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    $users = \App\Models\User::factory(10)
+      ->hasComments(3)
+      ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    $comments = \App\Models\Comment::get();
+    foreach ($comments as $comment) {
+      \App\Models\Reply::factory(rand(1, 3))->create([
+        'comment_id' => $comment->id,
+        'user_id' => rand(1, 10)
+      ]);
     }
+  }
 }
